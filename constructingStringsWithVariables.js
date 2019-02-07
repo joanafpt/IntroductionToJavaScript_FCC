5b37bf546f51735391d24a83b5f818e38aa34e8b
Joana <joanajteixeira@outlook.com>
2019-02-07 15:33:26 +0000

    //Basic JavaScript: Constructing Strings with Variables

---
+++ constructingStringsWithVariables.js
@@ -0,0 +1,12 @@
+//Basic JavaScript: Constructing Strings with Variables
+
+ //Set myName to a string equal to your name and build myStr with myName between the strings "My name is " and " and I am well!"
+
+ // Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Joana";
+var myStr = "My name is " + myName + "and I am well!";
+

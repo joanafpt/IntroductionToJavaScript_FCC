87ed03104c7d4a6b680a127fbd98501108b7dad1
Joana <joanajteixeira@outlook.com>
2019-02-07 17:56:39 +0000

    //Basic JavaScript: Understanding Undefined Value returned from a Function

---
+++ undefinedValueReturnedFromAFunction.js
@@ -0,0 +1,20 @@
+//Basic JavaScript: Understanding Undefined Value returned from a Function
+
+//Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive(){
+
+  sum += 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();

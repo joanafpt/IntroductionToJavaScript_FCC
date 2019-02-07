bc03882edb7a080851baf02131d985a812a4ee35
Joana <joanajteixeira@outlook.com>
2019-02-07 17:52:59 +0000

    //Basic JavaScript: Manipulate Arrays With push()

---
+++ manipulateArraysWith-push().js
@@ -0,0 +1,17 @@
+//Basic JavaScript: Manipulate Arrays With push()
+
+//Push ["dog", 3] onto the end of the myArray variable.
+
+//Note: .push() takes one or more parameters and "pushes" them onto the end of the array.
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]); 
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+
+myArray.push(["dog", 3]);

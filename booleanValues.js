4d1587a00ec3436b8674d25164778efc92598a4f
Joana <joanajteixeira@outlook.com>
2019-02-07 15:26:03 +0000

    //Basic JavaScript: Understanding Boolean Values

---
+++ booleanValues.js
@@ -0,0 +1,12 @@
+//Basic JavaScript: Understanding Boolean Values
+
+//Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
+
+function welcomeToBooleans() {
+
+    // Only change code below this line.
+    
+    return true; // Change this line
+    
+    // Only change code above this line.
+    }

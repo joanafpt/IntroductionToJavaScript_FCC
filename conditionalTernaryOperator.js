b474eac55f4a6ad04e491f50a3044d76973046bc
Joana <joanajteixeira@outlook.com>
2019-02-07 15:33:19 +0000

    //Basic JavaScript: Use the Conditional (Ternary) Operator

---
+++ conditionalTernaryOperator.js
@@ -0,0 +1,26 @@
+//Basic JavaScript: Use the Conditional (Ternary) Operator
+
+//Use the conditional operator in the checkEqual function to check if two numbers are equal or not. The function should return either true or false.
+
+
+function checkEqual(a, b) {
+
+return (a === b ? true : false);
+
+}
+
+checkEqual(1, 2); 
+
+
+
+/* OR
+
+function checkEqual(a, b) {
+
+var x = (a === b ? true : false);
+
+return x;
+
+}
+
+checkEqual(1, 2); */

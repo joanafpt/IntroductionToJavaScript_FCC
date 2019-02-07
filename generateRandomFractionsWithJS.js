206d89c524648caa6981ff8f81b7ef495c0ad857
Joana <joanajteixeira@outlook.com>
2019-02-07 17:50:55 +0000

    //Basic JavaScript: Finding a Remainder in JavaScript

---
+++ generateRandomFractionsWithJS.js
@@ -0,0 +1,16 @@
+//Basic JavaScript: Generate Random Fractions with JavaScript
+
+//Change randomFraction to return a random number instead of returning 0.
+
+function randomFraction() {
+
+    // Only change code below this line.
+  
+    var result = (Math.random() * 0.1)
+    return result;
+  
+    // Only change code above this line.
+  }
+  
+  
+  console.log(randomFraction());

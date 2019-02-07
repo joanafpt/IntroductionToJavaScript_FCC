4a41d0dbb3abd99f2370ac24d1ac3e1eb1fd1ef8
Joana <joanajteixeira@outlook.com>
2019-02-07 17:52:13 +0000

    //Basic JavaScript: Iterate with JavaScript Do...While Loops

---
+++ iterateWithJavaScriptSoWhileLoops.js
@@ -0,0 +1,20 @@
+//Basic JavaScript: Iterate with JavaScript Do...While Loops
+//Change the while loop in the code to a do...while loop so that the loop will push the number 10 to myArray, and i will be equal to 11 when your code finishes running.
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do {
+
+  myArray.push(i);
+  i++;
+}
+
+while (i <= 10);
+
+
+
+console.log(myArray);

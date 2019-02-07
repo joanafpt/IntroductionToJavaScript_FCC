144f92628d79ef56eabcb14628b492687702ffea
Joana <joanajteixeira@outlook.com>
2019-02-07 17:56:12 +0000

    //Basic JavaScript: Stand in Line

---
+++ storeMultipleValuesInOneVariableUsingJSArrays.js
@@ -0,0 +1,11 @@
+//Basic JavaScript: Store Multiple Values in one Variable using JavaScript Arrays
+
+//Modify the new array myArray so that it contains both a string and a number (in that order).
+
+
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Hello", 2.67];

9668a7e5a7cf94199b06b19f548cc06e2a270358
Joana <joanajteixeira@outlook.com>
2019-02-07 17:54:56 +0000

    // Basic JavaScript: Quoting Strings with Single Quotes

---
+++ quotingStringsWithSingleQuotes.js
@@ -0,0 +1,5 @@
+// Basic JavaScript: Quoting Strings with Single Quotes
+// Change the provided string to a string with single quotes at the beginning and end and no escape characters.
+
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';

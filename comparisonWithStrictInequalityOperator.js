dbd15e8366172c2fe711142e7b61517d578d074f
Joana <joanajteixeira@outlook.com>
2019-02-07 15:32:16 +0000

    //Basic JavaScript: Comparison with the Strict Equality Operator

---
+++ comparisonWithStrictInequalityOperator.js
@@ -0,0 +1,19 @@
+//Basic JavaScript: Comparison with the Strict Inequality Operator
+
+//Add the strict inequality operator to the if statement so the function will return "Not Equal" when val is not strictly equal to 17
+
+// Setup
+function testStrictNotEqual(val) {
+    // Only Change Code Below this Line
+    
+    if (val !== 17) {
+  
+    // Only Change Code Above this Line
+  
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  // Change this value to test
+  testStrictNotEqual(10);

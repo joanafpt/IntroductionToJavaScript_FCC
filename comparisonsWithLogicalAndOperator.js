4737ea8c627334e7608020aabb1d5f5ccccec14f
Joana <joanajteixeira@outlook.com>
2019-02-07 15:31:19 +0000

    //Basic JavaScript: Comparisons with the Logical And Operator

---
+++ comparisonsWithLogicalAndOperator.js
@@ -0,0 +1,18 @@
+//Basic JavaScript: Comparisons with the Logical And Operator
+
+//Combine the two if statements into one statement which will return "Yes" if val is less than or equal to 50 and greater than or equal to 25. Otherwise, will return "No".
+
+function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val >= 25 && val <= 50) {
+        return "Yes";
+      }
+    
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  // Change this value to test
+  testLogicalAnd(10);

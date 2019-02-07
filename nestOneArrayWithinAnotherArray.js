5125badc06079b1140658afb15a98507e38566ba
Joana <joanajteixeira@outlook.com>
2019-02-07 17:54:18 +0000

    //Basic JavaScript: Nest one Array within Another Array

---
+++ nestOneArrayWithinAnotherArray.js
@@ -0,0 +1,11 @@
+//Basic JavaScript: Nest one Array within Another Array
+
+//Create a nested array called myArray. - This is also called a Multi-dimensional Array.
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["ThisIsMyArray", "Hola"], [23, 45, 9999]];
+
+

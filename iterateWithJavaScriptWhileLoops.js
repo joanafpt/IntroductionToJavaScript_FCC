d92a280e01376ce7c95ccad1508e44c5f67ecbbe
Joana <joanajteixeira@outlook.com>
2019-02-07 17:52:20 +0000

    //Basic JavaScript: Iterate with JavaScript While Loops

---
+++ iterateWithJavaScriptWhileLoops.js
@@ -0,0 +1,18 @@
+//Basic JavaScript: Iterate with JavaScript While Loops
+
+//Push the numbers 0 through 4 to myArray using a while loop.
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+var counter = 0;
+
+while(counter <= 4){
+
+  myArray.push(counter);
+  counter ++;
+}
+
+console.log(myArray);

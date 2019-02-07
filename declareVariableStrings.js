e849dc10f903d8d84b14301fe534c0d58b339223
Joana <joanajteixeira@outlook.com>
2019-02-07 15:36:59 +0000

    //Basic JavaScript: Declare String Variables

---
+++ declareVariableStrings.js
@@ -0,0 +1,12 @@
+//Basic JavaScript: Declare String Variables
+
+//Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Joana";
+var myLastName= "Teixeira";

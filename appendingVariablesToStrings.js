573dc7798c3a99445d461359e34288bb2a56f79d
Joana <joanajteixeira@outlook.com>
2019-02-07 15:25:42 +0000

    //Basic JavaScript: Appending Variables to Strings

---
+++ appendingVariablesToStrings.js
@@ -0,0 +1,17 @@
+//Basic JavaScript: Appending Variables to Strings
+
+//Set someAdjective and append it to myStr using the += operator.
+
+
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "cool";
+var myStr = "Learning to code is " 
+
+myStr += someAdjective

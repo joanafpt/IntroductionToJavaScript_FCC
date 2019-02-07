88cecfd2e795160945d866f325a8cbcdf4da509b
Joana <joanajteixeira@outlook.com>
2019-02-07 17:53:19 +0000

    //Basic JavaScript: Manipulate Arrays With unshift()

---
+++ manipulatingComplexObjects.js
@@ -0,0 +1,30 @@
+//Basic JavaScript: Manipulating Complex Objects
+
+//Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [ 
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+    // Add record here
+  
+    ,{
+      "artist": "Me",
+      "title": "lalalala",
+      "release_year": 2010,
+      "formats": [
+      "CD",
+      "DVD",
+      "Blu-ray"  
+  ],
+  
+    }
+    ];

b4bc054eadbf13e96c3d34d10dd344ff979fe69b
Joana <joanajteixeira@outlook.com>
2019-02-07 15:37:43 +0000

    //Basic JavaScript: Introducing Else Statements

---
+++ elseStatements.js
@@ -0,0 +1,22 @@
+//Basic JavaScript: Introducing Else Statements
+//Combine the if statements into a single if/else statement.
+
+function testElse(val) {
+    var result = "";
+    // Only change code below this line
+    
+    if (val > 5) {
+      result = "Bigger than 5";
+    }
+    
+    else {
+      result = "5 or Smaller";
+    }
+    
+    // Only change code above this line
+    return result;
+  }
+  
+  // Change this value to test
+  testElse(4);
+  

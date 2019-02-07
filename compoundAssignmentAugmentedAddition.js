73d182100f6ad30391f9f5f4c94855a44183ecc0
Joana <joanajteixeira@outlook.com>
2019-02-07 15:32:26 +0000

    //Basic JavaScript: Compound Assignment With Augmented Addition

---
+++ compoundAssignmentAugmentedAddition.js
@@ -0,0 +1,16 @@
+//Basic JavaScript: Compound Assignment With Augmented Addition
+//Convert the assignments for a, b, and c to use the += operator.
+
+//note: there are operators which do both a mathematical operation and assignment in one step.
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+
+a += 12; //a = a + 12;
+b += 9; //b = 9 + b;
+c += 7; //c = c + 7;
+

fbc9b053b6794d2e944e3f23994a387e8a510de0
Joana <joanajteixeira@outlook.com>
2019-02-07 17:56:52 +0000

    //Basic JavaScript: Understanding Uninitialized Variables

---
+++ updatingObjectProperties.js
@@ -0,0 +1,28 @@
+//Basic JavaScript: Updating Object Properties
+//Update the myDog object's name property. Let's change her name from "Coder" to "Happy Coder". You can use either dot or bracket notation.
+
+
+//object.property = ""; || object[property] = "";
+
+// Example
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+  };
+  
+  ourDog.name = "Happy Camper";
+  
+  // Setup
+  var myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.name = "Happy Coder";
+  // Only change code below this line.
+  
+
